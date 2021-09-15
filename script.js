300a92776b86807376d77969edae47c877975e9b
unknown <mccullochryan636@gmail.com>
2021-09-15 15:58:39 -0600

    added comfirms and alets to follow prompt

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 // I defined my Variables for the pass word
-var generateBtn = document.querySelector("#generate");
-var upperLetter = ["A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" ]; 
-var lowerLetter = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z" ];
-var Special = ["!" , "@" , "#" , "$" , "%" , "^" , "&" ,  "*" , "(" , ")" , "_" , "+" , "=" , "-" , "?"];
-var number = ["1", "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0"];
+let generateBtn = document.querySelector("#generate");
+let upperLetter = ["A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z" ]; 
+let lowerLetter = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z" ];
+let Special = ["!" , "@" , "#" , "$" , "%" , "^" , "&" ,  "*" , "(" , ")" , "_" , "+" , "=" , "-" , "?"];
+let number = ["1", "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0"];
 
 function randomPassword() {
-var yourPassword = [""];
-var yourOptions = [""];
-var promptOptions = prompt("select a number between " + 8 + " and " + 128)
+let yourPassword = [];
+let yourOptions = [];
+let promptOptions = prompt("select a number between " + 8 + " and " + 128); 
+
+if(promptOptions < 8 || promptOptions === "" || promptOptions > 128 || promptOptions === NaN){
+  alert("Please Follow Directions.");
+  return;
+};
+
+let findUpperletter = confirm("Would you like an Upper Case?");
+
+let findlowerletter = confirm("Would you like a lower Case?");
+
+let findSpecial = confirm("Would you like a Special Character?");
+
+let findNumber = confirm("Would you like a number?");
+
+
+if(!findUpperletter && !findlowerletter && !findSpecial && !findNumber) {
+  alert("Please select choose an option");
+  return;
+}
+
+if(findUpperletter === true){
+  yourPassword.push
+}
+
+
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector(password);
+  let password = randomPassword();
+  let passwordText = document.querySelector(password);
 
   passwordText.value = password;
 

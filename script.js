a9acd36a3ff99b9ae563e7ce4df24b28132774d0
unknown <mccullochryan636@gmail.com>
2021-09-14 18:27:18 -0600

    created variable and functions to create the password

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-// Assignment Code
+// I defined my Variables for the pass word
 var generateBtn = document.querySelector("#generate");
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var specialChar = "!@#$%^&*()_+=-`?"
+var number = "1234567890"
+
+//I created Functions for create the password
+function getLowercase() {
+  return lowerCase[Math.floor(Math.random() * lowerCase.length)];
+}
+
+function getUppercase() {
+  return upperCase[Math.floor(Math.random() * upperCase.length)];
+}
+
+function getNumber() {
+  return number[Math.floor(Math.random() * number.length)];
+}
+
+function getSymbol() {
+  return specialChar[Math.floor(Math.random() * specialChar.length)];
+}
 
 
 // Write password to the #password input
